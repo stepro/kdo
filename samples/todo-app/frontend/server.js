f9912bac28da99c03f120ff4f99861058b8f7cd0
Stephen Provine <stephpr@microsoft.com>
2020-06-07 15:20:44 -0700

    Fix todo app startup

--- samples/todo-app/frontend/server.js
+++ samples/todo-app/frontend/server.js
@@ -38,6 +38,10 @@ app.prepare().then(() => {
 
     var bus = serviceBus.bus({ url: process.env.STATS_QUEUE_URI });
     bus.use(bus.logger());
+    bus.on("error", err => {
+        console.error(err.message)
+        process.exit(1);
+    })
 
     function updateStats(updateEvent) {
         console.log("Pushing stats update: " + updateEvent);
